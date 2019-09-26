148fe7d951c2b4661f39f783a8d00e7ee6441307
robsonklima <robsonklima@gmail.com>
2019-09-26 19:49:36 -0300

    REF

--- src/pages/indicadores-filial/reincidencia-filial.ts
+++ src/pages/indicadores-filial/reincidencia-filial.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Chart } from "chart.js";
 
 
 @Component({
@@ -8,11 +9,61 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ReincidenciaFilialPage {
   nomeFilial: string;
+  @ViewChild("doughnutCanvas") doughnutCanvas: ElementRef;
+  private doughnutChart: Chart;
   
   constructor(
     private nav: NavController,
     private navParams: NavParams
   ) {
     this.nomeFilial = this.navParams.get('nomeFilial');
+  }
+
+  ngOnInit() {
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: "doughnut",
+      data: {
+        labels: ["A", "B", "C", "D", "E", "F"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"]
+          }
+        ]
+      },
+      options: {
+        legend: {
+            position: 'left',
+            display: true,
+            labels: {
+              boxWidth: 12,
+              padding: 10
+            }
+        },
+        tooltips: {
+          callbacks: {
+              label: function(tooltipItem, data) {
+                  var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+                  if (label) {
+                      label += ': ';
+                  }
+                  label += Math.round(tooltipItem.yLabel * 100) / 100;
+                  
+                  return label;
+              }
+          }
+      }
+      }
+    });
   }
 }
--- src/pages/indicadores-filial/sla-filial.ts
+++ src/pages/indicadores-filial/sla-filial.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, ElementRef} from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Chart } from "chart.js";
 
 
 @Component({
@@ -8,11 +9,58 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SLAFilialPage {
   nomeFilial: string;
+  @ViewChild("barCanvas") barCanvas: ElementRef;
+  private barChart: Chart;
   
   constructor(
-    private navCtrl: NavController,
     private navParams: NavParams
   ) {
     this.nomeFilial = this.navParams.get('nomeFilial');
+  }
+
+  ngOnInit() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "Qtd",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
   }
 }
