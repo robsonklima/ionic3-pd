e72564bb22192416d6e6f18d8582f6246d0ea079
robsonklima <robsonklima@gmail.com>
2020-08-14 10:03:19 -0300

    Https

--- src/models/config.ts
+++ src/models/config.ts
@@ -32,5 +32,5 @@ export class Config {
   };
 
   //public static API_URL = 'http://localhost:60687/api/';
-  public static API_URL = 'http://sat.perto.com.br/prjSATWebAPI/api/';
+  public static API_URL = 'https://sat.perto.com.br/prjSATWebAPI/api/';
 }
