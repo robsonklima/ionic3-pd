00de0cc92e1c48d84e79d834cb8c965740b56ee0
robsonklima <robsonklima@gmail.com>
2020-09-15 19:59:13 -0300

    REF

--- src/pages/disponibilidade-regioes/disponibilidade-regioes.ts
+++ src/pages/disponibilidade-regioes/disponibilidade-regioes.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild, ElementRef} from '@angular/core';
-import { LoadingController, NavController, AlertController } from 'ionic-angular';
-
+import { Component} from '@angular/core';
+import { DisponibilidadeRegiao } from '../../models/disponibilidade-regiao';
 import { DisponibilidadeRegiaoService } from '../../services/disponibilidade-regiao';
 
 
@@ -14,15 +13,50 @@ import { DisponibilidadeRegiaoService } from '../../services/disponibilidade-reg
     </ion-header>
 
     <ion-content>
+      <ion-card *ngFor="let d of disponibilidades; let i = index">
+
+        <img src="../../assets/imgs/card.png">
+        <ion-fab right top>
+          <button ion-fab>
+            <ion-icon name="pin"></ion-icon>
+          </button>
+        </ion-fab>
       
+        <ion-item>
+          <ion-icon name="football" item-start large></ion-icon>
+          <h2>{{ d.Regiao }}</h2>
+          <p>11 N. Way St, Madison, WI 53703</p>
+        </ion-item>
+      
+        <ion-item>
+          <ion-icon name="wine" item-start large ></ion-icon>
+          <h2>Institute of Fine Cocktails</h2>
+          <p>14 S. Hop Avenue, Madison, WI 53703</p>
+        </ion-item>
+      
+        <ion-item>
+          <span item-start>18 min</span>
+          <span item-start>(2.6 mi)</span>
+          <button ion-button icon-start clear item-end>
+            <ion-icon name="navigate"></ion-icon>
+            Start
+          </button>
+        </ion-item>
+      </ion-card>
     </ion-content>`
 })
 export class DisponibilidadeRegioesPage {
+  disponibilidades: DisponibilidadeRegiao[] = [];
+
   constructor(
     private disponibilidadeRegiaoService: DisponibilidadeRegiaoService,
   ) {}
 
   ionViewWillEnter() {
-    
+    this.disponibilidadeRegiaoService.buscarDisponibilidadeRegioes()
+      .subscribe((dados: DisponibilidadeRegiao[]) => {
+        this.disponibilidades = dados;
+      }, 
+      err => {});
   }
 }
